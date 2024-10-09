84e952ddd5732c26ba66b8465af8a395800920e1
yryskeldi0101 <yryskeldi.9121@gmail.com>
2024-10-09 18:11:53 +0600

    database posgres

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
 
 app = FastAPI()
 
+DATABASE_URL = "postgresql://postgres:yryskeldi@localhost:5432/hr_backend"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/items/")
+async def create_item(name: str, db: Session = Depends(get_db)):
+    new_item = Item(name=name)
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, db: Session = Depends(get_db)):
+    return db.query(Item).filter(Item.id == item_id).first()
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int, db: Session = Depends(get_db)):
+    return db.query(Item).filter(Item.id == item_id).delete()
